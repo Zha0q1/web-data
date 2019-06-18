74d105d0e09f7471729a102ae86d3d89b335a596
Haibin Lin <linhaibin.eric@gmail.com>
2019-06-17 17:21:00 -0700

    Update finetune_squad2.0_large_mx1.5.0b20160216.sh

--- gluonnlp/logs/bert/finetune_squad2.0_large_mx1.5.0b20160216.sh
+++ gluonnlp/logs/bert/finetune_squad2.0_large_mx1.5.0b20160216.sh
@@ -1,3 +1,7 @@
+# training
 python finetune_squad.py --bert_model bert_24_1024_16 --optimizer adam --accumulate 8 \
                          --batch_size 4 --lr 3e-5 --epochs 2 --gpu 0 --null_score_diff_threshold -2.0 \
                          --version_2
+
+# evaluation
+# python evaluate-v2.0.py dev-v2.0.json predictions.json
